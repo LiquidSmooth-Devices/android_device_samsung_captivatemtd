96f5d286da717e94e7389a7906892670140f7722
Tony Malagisi <mtb3000gt@gmail.com>
2014-08-17 14:48:15 -0400

    Use performance kernel

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -18,7 +18,7 @@
 #
 
 # Kernel Config
-TARGET_KERNEL_CONFIG := cyanogenmod_captivatemtd_defconfig
+TARGET_KERNEL_CONFIG := liquidsmooth_captivatemtd_defconfig
 
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/captivatemtd/bluetooth
